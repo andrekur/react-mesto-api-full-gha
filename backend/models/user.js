5c36e388c56eb496a11184a4b64fc4bdeb893864
andre <kurepin.andre@yandex.ru>
2024-01-08 22:07:48 +0300

    Изменяет логику работы модели

--- backend/models/user.js
+++ backend/models/user.js
@@ -8,21 +8,18 @@ const { UnauthorizedError } = require('../errors/errors')
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто'
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Исследователь'
   },
   avatar: {
     type: String,
-    required: true,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator: (v) => isUrl(v),
@@ -41,7 +38,6 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    minlength: 8,
     select: false
   }
 });
